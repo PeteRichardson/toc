3236334cde72463b74bb1cadf9f567f38bb034fc
Pete Richardson <pete@peterichardson.com>
2024-06-18 15:34:59 -0700

    Change from counting lines to finding section headers using simple regex

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
 //use toc::parse_log;
-use std::path::Path;
+use std::path::PathBuf;
 use std::error::Error;
 use std::io::{BufReader, BufRead};
 use std::fs::File;
 use log::info;
+use regex::Regex;
 
-fn count_lines(path: &Path) -> Result<usize, std::io::Error> {
-    info!("counting '{:?}'", path);
-    let mut lines = BufReader::new(File::open(path)?).lines();
-    let count = lines.try_fold(0, |acc, line| line.map(|_| acc + 1))?;
-    Ok(count)
+fn find_lines(path: &PathBuf, re: Regex) -> Result<Vec<String>, std::io::Error> {
+    info!("searching '{:?}' for '{}'", path, re);
+    let file = BufReader::new(File::open(path)?);
+    let matches : Vec<String> = file.lines()
+        .map(|i| i.unwrap())
+        .filter(|l| re.is_match(l))
+        .collect();
+    Ok(matches)
 }
-
 
 fn main() -> Result<(), Box<dyn Error>> {
 
     env_logger::init();
 
     const FILENAME: &str = "dlog0.log";
-    let filepath = Path::new(FILENAME);
-    let count: usize = count_lines(filepath).expect("Couldn't count lines!");
-    println!("{} - {} lines", FILENAME, count);
-
+    let filepath = PathBuf::from(FILENAME);
+    let pattern = Regex::new(r"^[\+]+ (Section [\d\.]+)")?;
+    let lines: Vec<String> = find_lines(&filepath, pattern)?;
+    for line in lines {
+        println!("{}", line);
+    }
     Ok(())
 }
