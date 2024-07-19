6f138b5b53a28434a46f1e649ff03f66710b63c9
Pete Richardson <pete@peterichardson.com>
2024-07-18 19:05:00 -0700

    Change TocEntry from tuple to a struct
    so we can define a custom fmt::Display for it

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
 use log::info;
 use regex::Regex;
 use std::error::Error;
+use std::fmt;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::path::PathBuf;
 
 // A table of contents entry is a tuple of line offset and the line itself
-type TocEntry = (usize, String);
+struct TocEntry {
+    offset: usize,
+    line: String,
+}
+
+impl TocEntry {
+    fn new(offset: usize, line: String) -> TocEntry {
+        TocEntry { offset, line }
+    }
+}
+
+impl fmt::Display for TocEntry {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "line offset {:3?} - {}", self.offset, self.line)
+    }
+}
 
 fn extract_toc_entries(lines: Vec<String>, re: Regex) -> Vec<TocEntry> {
     // Search for a regex pattern in a vector of strings
@@ -15,7 +31,8 @@ fn extract_toc_entries(lines: Vec<String>, re: Regex) -> Vec<TocEntry> {
     let mut toc_entries: Vec<TocEntry> = Vec::new();
     for (offset, line) in lines.iter().enumerate() {
         if re.is_match(line) {
-            toc_entries.push((offset, line.clone()));
+            let entry = TocEntry::new(offset, line.clone());
+            toc_entries.push(entry);
         }
     }
     toc_entries
@@ -39,7 +56,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let toc_entries: Vec<TocEntry> = extract_toc_entries(lines, pattern);
 
     for entry in toc_entries {
-        println!("line offset {:3?} - {}", entry.0, entry.1);
+        println!("{}", entry);
     }
     Ok(())
 }
